40678a06454397e46de760da5fa8a42564e414a2
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-08-17 12:24:43 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.py
@@ -0,0 +1,46 @@
+
+from typing import List
+
+
+class Solution:
+    def largest(self, n : int, arr : List[int]) -> int:
+        max=arr[0]
+        for i in range(len(arr)):
+            if(arr[i]>max):
+                max=arr[i]
+        return max
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        arr = IntArray().Input(n)
+
+        obj = Solution()
+        res = obj.largest(n, arr)
+
+        print(res)
+
+# } Driver Code Ends
