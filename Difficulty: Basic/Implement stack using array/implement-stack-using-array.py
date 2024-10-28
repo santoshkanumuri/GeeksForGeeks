e9ac8ef02e2adbba273d17faef0f819850a31900
Santosh Kanumuri <73770283+santoshkanumuri@users.noreply.github.com>
2024-10-28 10:35:08 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+
+class MyStack:
+    
+    def __init__(self):
+        self.arr=[]
+    
+    #Function to push an integer into the stack.
+    def push(self,data):
+        self.arr.append(data)
+        #add code here
+    
+    #Function to remove an item from top of the stack.
+    def pop(self):
+        #add code here
+        if self.arr:
+            return self.arr.pop()
+        return -1
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    T = int(data[0])
+    idx = 1
+    for _ in range(T):
+        sq = MyStack()
+        line = data[idx].strip()
+        nums = list(map(int, line.split()))
+        idx += 1
+        n = len(nums)
+        i = 0
+        while i < n:
+            QueryType = nums[i]
+            i += 1
+            if QueryType == 1:
+                a = nums[i]
+                i += 1
+                sq.push(a)
+            elif QueryType == 2:
+                print(sq.pop(), end=" ")
+        print()
+        print("~")
+
+# } Driver Code Ends
